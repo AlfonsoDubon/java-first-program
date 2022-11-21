e98a59a9261b9e2a3619025a500693b16edff7bf
Alfonso Dubon <65038872+AlfonsoDubon@users.noreply.github.com>
2022-11-21 14:52:47 -0600

    Completed module

---
+++ src/main/java/com/h2/SavingCalculator.java
@@ -0,0 +1,16 @@
+package com.h2;
+
+public class SavingCalculator {
+
+    private float[] credits;
+    private float[] debits;
+
+
+    public SavingCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+    private float sumOfCredits(){
+        return 0.0f;
+    }
+}
